428864ca528cc180cea9050edb6f86e842d09bb3
Elizaveta Lebedeva <elizaveta202@yandex.ru>
2017-02-05 23:07:45 +0300

    list of file's attributes

---
+++ file_about.py
@@ -0,0 +1,20 @@
+import os
+
+main_table = []
+
+for filename in os.listdir("D:\\file_table"):
+    info = os.stat(filename)
+    size = info.st_size
+    path = os.path.realpath(filename)
+    fullname = os.path.basename(path)
+    fullname = fullname.split(".")
+    file_inf = []
+    file_inf.append(fullname[0])
+    file_inf.append(fullname[1])
+    file_inf.append(size)
+    file_inf.append(path)
+
+
+    main_table.append(file_inf)
+
+print (main_table)
