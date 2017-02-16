15a1ba70358029e2811403d28d1b32b619c89054
Elizaveta Lebedeva <elizaveta202@yandex.ru>
2017-02-17 00:26:28 +0300

    Filtering added
    fixed #5

--- file_about.py
+++ file_about.py
@@ -1,39 +1,53 @@
-import sys, os, HTML
+import sys, os, HTML, argparse, re
 
-main_table = []
+def table(path_, exc):
+    #pattern = re.compile("*.4", re.IGNORECASE)
 
-path_ = sys.argv[1]
+    
+    main_table = []
+    
+
+    #path_ = sys.argv[1]
                     
-for filename in os.listdir(path_):
+    for filename in os.listdir(path_):
     
-    path = path_+"\\"+filename
-    #print ("fullpath " +path)
-    size = os.path.getsize(path)
+        path = path_+"\\"+filename
+        #print ("fullpath " +path)
+        size = os.path.getsize(path)
     
-    fullname = os.path.basename(path)
-    fullname = fullname.split(".")
+        fullname = os.path.basename(path)
+        
+        if not re.search(exc, fullname):
+            file_inf = []
+            fullname = fullname.split(".")
+            file_inf.append(fullname[0])
+            file_inf.append(fullname[1])
+            file_inf.append(size)
+            file_inf.append(path)
+
+
+            main_table.append(file_inf)
+
+    htmlcode = str(HTML.table(main_table,header_row=['Name',   'Extension',   'Size (Bytes)', 'Path']))
+    f = open("file's table.htm", "w")
+
+    for i in htmlcode:
+        f.write(i)
+    f.close()
     
-    file_inf = []
-    file_inf.append(fullname[0])
-    file_inf.append(fullname[1])
-    file_inf.append(size)
-    file_inf.append(path)
-
-
-    main_table.append(file_inf)
-
-
-
-htmlcode = str(HTML.table(main_table,header_row=['Name',   'Extension',   'Size (Bytes)', 'Path']))
-
-
-f = open("file's table.htm", "w")
-
-for i in htmlcode:
-    f.write(i)
-f.close()
-    
-print ("The table has been created. You can find it in the same directory as this script.")
+    print ("The table has been created. You can find it in the same directory as this script.")
 
     
+parser = argparse.ArgumentParser()
+parser.add_argument("dir", help = "create html-table with information about the files in target directory")
+parser.add_argument("--exclude", action="store", help="exclude files with pointed directory")
+args = parser.parse_args()
+
+if args.exclude:
+    table(args.dir, args.exclude)
+    
+else:
+    
+    table(args.dir, args.exclude)
+
 
