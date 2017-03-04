b3429969a5ce99c3ab72171a56af1366ff458623
Elizaveta Lebedeva <elizaveta202@yandex.ru>
2017-03-04 18:49:19 +0300

    using pathlib
    close #6

--- file_about.py
+++ file_about.py
@@ -1,38 +1,33 @@
 import sys, os, HTML, argparse, re
-from path import Path
 
-def table(path_, exc):
-    
-    main_table = []
 
-    target_dir = Path(path_)
-    
+from pathlib import Path
 
-    for f in target_dir.walkfiles():
-    	fname = f.basename()
-  
-    	ext = fname.split(".")[-1]
-    	fname = fname.split(".")[0:-1]
-    	name ="".join(fname)
-    	
-    	size = f.getsize()
-    	path = os.path.join(path_, f.basename())
-    	
+main_table = []
 
-    	if exc and not re.search(exc, name) or not exc:
+def iter_dir (target_dir, exc):
+
+    for x in target_dir.iterdir():
+        if not x.is_dir():
+			
+            if exc and not re.search(exc, x.name) or not exc:
                 file_inf = []
-            
-                file_inf.append(name)
-                file_inf.append(ext)
-                file_inf.append(size)
-                file_inf.append(path)
+
+                file_inf.append(x.stem)
+                file_inf.append(x.suffix)
+                file_inf.append(x.stat().st_size)
+                file_inf.append(target_dir/x.name)
     
 
                 main_table.append(file_inf)
-    	
+        else:
+            iter_dir(x,exc)
 
+def table(path_, exc):
     
-
+    target_dir = Path(path_)
+    iter_dir (target_dir, exc)
+    
     htmlcode = str(HTML.table(main_table,header_row=['Name',   'Extension',   'Size (Bytes)', 'Path']))
     f = open("file's table.htm", "w")
 
